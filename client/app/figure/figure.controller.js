2ffed7a9b300ffd33ab7681aa063e08868b53e8d
Anthony Cocco <tcgtony@gmail.com>
2014-11-03 14:52:16 -0500

    added the graph picker and what not.

--- client/app/figure/figure.controller.js
+++ client/app/figure/figure.controller.js
@@ -39,6 +39,8 @@ angular.module('figureApp')
         $scope.editor.setValue(val);
     };
 
+
+    // Control methods
     $scope.setRawView = function(rawView) {
         $scope.rawView = rawView;
 
@@ -46,6 +48,10 @@ angular.module('figureApp')
         if (false) {
             $('#raw-data-table').trigger('updateAll', [true]);
         }
+    };
+
+    $scope.changeActiveGraph = function(graph) {
+        $scope.activeGraph = graph;
     };
 
     // Innate column sorting breaks with a key has a space in it. To avoid this,
@@ -66,12 +72,12 @@ angular.module('figureApp')
         Papa.parse(input, {
             header: true,
             dynamicTyping: true,
-            step: function(row, handle) {
+            worker: true,
+            step: function(row) {
                 if (row.errors.length > 0) {
                     for (var error in row.errors) {
                         $scope.parseError += row.errors[error].message + '\n';
                     }
-                    handle.abort();
                 }
                 else {
                     $scope.parsedData.push(row.data[0]);
@@ -102,4 +108,29 @@ angular.module('figureApp')
         // Tell angular to re-up.
         $scope.$apply();
     }
+
+    // Graph information
+    $scope.activeGraph = '';
+    $scope.graphTypes = [{
+        type: 'discreteBar',
+        formalName: 'Bar Chart',
+        learnMore: 'https://en.wikipedia.org/wiki/Bar_chart',
+        description: 'A bar chart or bar graph is a chart with rectangular bars with lengths proportional to the values that they represent. The bars can be plotted vertically or horizontally. A vertical bar chart is sometimes called a column bar chart.'
+
+    }, {
+        type: 'line',
+        formalName: 'Line Chart',
+        learnMore: 'https://en.wikipedia.org/wiki/Line_chart',
+        description: 'A line chart or line graph is a type of chart which displays information as a series of data points called "markers" connected by straight line segments. Line Charts show how a particular data changes at equal intervals of time. '
+    }, {
+        type: 'pie',
+        formalName: 'Pie Chart',
+        learnMore: 'https://en.wikipedia.org/wiki/Pie_chart',
+        description: 'A pie chart is divided into sectors, illustrating numerical proportion. In a pie chart, the arc length of each sector (and consequently its central angle and area), is proportional to the quantity it represents.'
+    }, {
+        type: 'scatter',
+        formalName: 'Scatterplot',
+        learnMore: 'https://en.wikipedia.org/wiki/Scatter_plot',
+        description: 'A scatter plot, scatterplot, or scattergraph is a type of mathematical diagram using Cartesian coordinates to display values for two variables for a set of data. The data is displayed as a collection of points, each having the value of one variable determining the position on the horizontal axis and the value of the other variable determining the position on the vertical axis.'
+    }];
   });
